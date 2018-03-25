8d73d43210005835440c0683ef8db692c99c6955
Max Frontend <maxfarseer@gmail.com>
2018-03-25 12:59:25 +0300

    draft (App) browsherHistory router

--- src/App.js
+++ src/App.js
@@ -1,38 +1,66 @@
-import React, { Component } from 'react'
-import Main from './Main'
-import logo from './logo.svg'
+import React from 'react'
+import { Route, Link } from './Router'
 import './App.css'
 
-class App extends Component {
-  changeSeason = e => {
-    const season = e.currentTarget.dataset.photoName
-    window.location.hash = season
-  }
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Stardew valley: сезоны + hash роутер</h1>
-        </header>
-        <div className="buttons">
-          <button data-photo-name="winter" onClick={this.changeSeason}>
-            Зима
-          </button>
-          <button data-photo-name="spring" onClick={this.changeSeason}>
-            Весна
-          </button>
-          <button data-photo-name="summer" onClick={this.changeSeason}>
-            Лето
-          </button>
-          <button data-photo-name="autumn" onClick={this.changeSeason}>
-            Осень
-          </button>
-        </div>
-        <Main />
-      </div>
-    )
-  }
+const Home = () => <h2>Home</h2>
+
+const About = () => <h2>About</h2>
+
+const Topic = ({ topicId }) => <h3>{topicId}</h3>
+
+const Topics = ({ match }) => {
+  const items = [
+    { name: 'Rendering with React', slug: 'rendering' },
+    { name: 'Components', slug: 'components' },
+    { name: 'Props v. State', slug: 'props-v-state' },
+  ]
+
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        {items.map(({ name, slug }) => (
+          <li key={name}>
+            <Link to={`${match.url}/${slug}`}>{name}</Link>
+          </li>
+        ))}
+      </ul>
+      {items.map(({ name, slug }) => (
+        <Route
+          key={name}
+          path={`${match.path}/${slug}`}
+          render={() => <Topic topicId={name} />}
+        />
+      ))}
+      <Route
+        exact
+        path={match.url}
+        render={() => <h3>Please select a topic.</h3>}
+      />
+    </div>
+  )
 }
+
+const App = () => (
+  <div>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/topics">Topics</Link>
+      </li>
+    </ul>
+
+    <hr />
+
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/topics" component={Topics} />
+  </div>
+)
 
 export default App
