118e4bd1482a4c5a72b4a8f71f267ebbb8082257
Max Frontend <maxfarseer@gmail.com>
2018-04-08 19:56:07 +0300

    add from twitch

--- src/components/ReactSelect.js
+++ src/components/ReactSelect.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import { Creatable } from 'react-select'
+import 'react-select/dist/react-select.css'
+
+const myOptions = [
+  { value: 'one', text: 'One' },
+  { value: 'two', text: 'Two' },
+  { value: 'stream', text: 'hello, twitch' },
+]
 
 class ReactSelect extends React.Component {
+  state = {
+    value: [],
+  }
+  handleChange = value => {
+    // на бэкэнд отправляй только value
+    this.setState({ value })
+  }
   render() {
-    return <div>React select here</div>
+    const { value } = this.state
+
+    return (
+      <Creatable
+        name="form-field-name"
+        value={value}
+        multi
+        onChange={this.handleChange}
+        options={myOptions}
+        labelKey={'text'}
+      />
+    )
   }
 }
 
